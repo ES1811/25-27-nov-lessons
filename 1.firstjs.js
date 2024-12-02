d3faafcac6a8cb04a0954df36c34ffa064ddc4ff
ES1811 <es1811.kodehode@gmail.com>
2024-12-02 09:41:42 +0100

    Update 1.firstjs.js

--- 1.firstjs.js
+++ 1.firstjs.js
@@ -1,5 +1,5 @@
-let myName = "Elisabeth";
-let myCity = "Oslo";
+let myName = "Eli";
+let myCity = "New York";
 let myHobbies = "IT";
 
 let personalInfo = "person name:" +myName+"\n\n"+  "person city: "+myCity+"\n\n"+ "person hobby: "+myHobbies;
